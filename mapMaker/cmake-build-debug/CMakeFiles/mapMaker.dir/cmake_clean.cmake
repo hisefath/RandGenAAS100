5c95d253919e79a07ea8eb0238839285d43f254b
Mohammed Sefath Chowdhury <Mchowdhury57@gmail.com>
2017-12-08 22:31:51 -0500

    Update

---
+++ mapMaker/cmake-build-debug/CMakeFiles/mapMaker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mapMaker.dir/main.cpp.o"
+  "mapMaker.pdb"
+  "mapMaker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
